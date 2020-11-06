8507c6b91cff980b6b5d9d8f6dce47f31734669c
Edgar Tec <edgartec87@gmail.com>
2020-11-06 13:01:15 -0500

    Se agregaron las rutas con soporte multi-idiomas donde cada URL tiene un prefijo con el código del idioma excepto el idioma por defecto del sitio

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,25 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Language;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+$locale = request()->segment(1);
+$locale_codes = Language::getAvailableLocaleCodes();
+$default_locale = config('localization.site_default_locale');
 
-Route::get('/', function () {
-    return view('welcome');
+if (in_array($locale, $locale_codes) && $locale != $default_locale && config('localization.enable_multi_language_site')) {
+    App::setLocale($locale);
+} else {
+    App::setLocale($default_locale);
+    $locale = null;
+}
+
+Route::group(array('prefix' => $locale), function () {
+    Route::get('/contact-us', function () {
+        return view('contact-us');
+    })->name('page.contact_us');
+    Route::get('/', [PostController::class, 'index'])->name('blog.home');
+    Route::get('/{slug}', [PostController::class, 'show'])->name('blog.post');
 });
