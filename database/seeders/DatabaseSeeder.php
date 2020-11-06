f0a01b260d3b4e38fa1e57323ea8ccb3a1cb99c5
Edgar Tec <edgartec87@gmail.com>
2020-11-06 12:36:33 -0500

    Se agregaron los seeders de Language y PostTranslation al DatabaseSeeder para poder ejecutarlos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(LanguageSeeder::class);
+        $this->call(PostTranslationSeeder::class);
     }
 }
