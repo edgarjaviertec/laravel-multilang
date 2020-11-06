a7646817fbd63b68d7f9315e7094c81700c01434
Edgar Tec <edgartec87@gmail.com>
2020-11-06 12:47:30 -0500

    Se creó un helper para generar las URLs de la lista desplegable de idiomas

---
+++ app/helpers.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\Language;
+use App\Models\Setting;
+
+if (!function_exists('get_language_switcher_url')) {
+    function get_language_switcher_url($locale)
+    {
+        $locale_codes = Language::getAvailableLocaleCodes();
+        $default_locale = config('localization.site_default_locale');
+        $segments = Request::segments();
+        if ($segments && in_array($segments[0], $locale_codes)) {
+            unset($segments[0]);
+        }
+        if ($locale != $default_locale) {
+            $segments = Arr::prepend($segments, $locale);
+        }
+        $url = '/' . implode('/', $segments);
+        return $url;
+    }
+}
