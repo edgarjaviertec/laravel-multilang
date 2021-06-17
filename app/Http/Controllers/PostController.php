1b349b365632feb00de0ea37b470f98df61b2584
Edgar Tec <edgartec87@gmail.com>
2021-06-17 17:10:53 -0500

    Se refactorizó el código

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,8 +9,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::latest()->get();
-        return view('posts')
-            ->with('posts', $posts);
+        return view('posts')->with('posts', $posts);
     }
 
     public function show($slug)
@@ -19,7 +18,6 @@ class PostController extends Controller
         if ($post->translate()->where('slug', $slug)->first()->locale != app()->getLocale()) {
             return redirect()->route('blog.post', $post->translate()->slug);
         }
-        return view('post')
-            ->with('post', $post);
+        return view('post')->with('post', $post);
     }
 }
